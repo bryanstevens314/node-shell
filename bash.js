b39410596ef5f5ca9075ad26eb7bb3ce98c62105
Bryan Stevens <ryan_stevens314@yahoo.com>
2018-11-06 11:39:51 -0800

    added some commands

---
+++ bash.js
@@ -0,0 +1,12 @@
+let commands = {
+  pwd: process.cwd
+};
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if (commands[cmd]) {
+    process.stdout.write(commands[cmd]());
+    process.stdout.write('\nprompt > ');
+  }
+
+});
